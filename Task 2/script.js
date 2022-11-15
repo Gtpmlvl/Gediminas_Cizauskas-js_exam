eabb71a289ef45b5780e9c3fe2c15e2f687e2149
Gtpmlvl <cizauskasgediminas@gmail.com>
2022-11-15 11:38:28 +0200

    2 uzduotis

--- Task 2/script.js
+++ Task 2/script.js
@@ -3,5 +3,20 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+"use strict";
+console.log("script.js");
 
-// antras update
+// let clicks = 0;
+
+// function counter() {
+//   clicks += 1;
+// }
+const btnEl = document.getElementById("btn__element");
+const countRez = document.getElementById("btn__state");
+
+let count = 0;
+
+btnEl.onclick = function () {
+  count += 1;
+  countRez.innerHTML = count;
+};
